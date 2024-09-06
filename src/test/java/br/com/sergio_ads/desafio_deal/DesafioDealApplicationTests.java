73d665e6f72a8ac683ace68ac7721311375139b1
Sérgio Guimarães de Andrade <sergio.guimaraes.ads@gmail.com>
2024-09-06 11:44:18 -0300

    Created project

---
+++ src/test/java/br/com/sergio_ads/desafio_deal/DesafioDealApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.sergio_ads.desafio_deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Teste contexto
+	}
+
+}
